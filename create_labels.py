fb8fdf963429d8c6260c2eaf9f4750eb0ef79bfc
sarah-inteceleri <sarah.vieira@gedu.demo.inteceleri.com.br>
2024-11-03 17:20:26 -0300

    fix(semec): posições e desempenho das etiquetas

--- create_labels.py
+++ create_labels.py
@@ -10,7 +10,7 @@ import io
 # Estilo do texto nas etiquetas
 paragraph_label_style = ParagraphStyle(
     'paragraph labels style', 
-    fontSize=10,
+    fontSize=11,
     borderPadding=5,
     alignment=TA_CENTER
 )
@@ -18,13 +18,13 @@ paragraph_label_style = ParagraphStyle(
 # Função para desenhar uma única etiqueta
 def desenhar_etiqueta(c, x, y, largura, altura, tabela, logo, championship, stage):
     # Definir a cor da borda para branco
-    ##c.setStrokeColorRGB(1, 1, 1)  # Branco
+    c.setStrokeColorRGB(1, 1, 1)  # Branco
     
     # Desenho do retângulo da etiqueta
     c.rect(x, y, largura, altura)
     
     # Inserção do logo
-    c.drawImage(logo, x + (largura - (85.9 * mm)) / 2, y + altura - 45, width=(85.9 * mm), height=(14.49 * mm))
+    c.drawImage(logo, x + (1 * mm), y + altura - (14.9 * mm), width=(largura - (2 * mm)), height=(14.9 * mm))
     
     # Texto da etiqueta
     p = Paragraph(f"""
@@ -45,7 +45,7 @@ def gerar_etiquetas(tabela, logo, championship, stage):
     largura_pagina, altura_pagina = A4
     largura_etiqueta = 99 * mm
     altura_etiqueta = 55 * mm  # Ajuste para altura exata
-    margem_topo = 9 * mm
+    margem_topo = 10 * mm
     margem_lateral = 5 * mm
     espaco_vertical = 3 * mm  # Espaço vertical entre as colunas
     
@@ -60,30 +60,25 @@ def gerar_etiquetas(tabela, logo, championship, stage):
     # Posições das colunas
     x_positions = [margem_lateral, largura_pagina / 2 + espaco_vertical / 2]
     y_position = altura_pagina - margem_topo - altura_etiqueta
-
+    
     etiquetas_na_pagina = 0
     linha = 0
+    etiqueta_positions = x_positions[0]
     
+    # Loop  que executa para cada linha na tabela. Desenha a etiqueta na posicao 1 ou dois com base nos indices de x_position. Verifica se tem 10 etiquetas na pagina, se houver, finaliza a pagina e reseta as posicoes para comecar uma nova pagina.
     for index, row in tabela.iterrows():
-        for x in x_positions:
-            desenhar_etiqueta(c, x, y_position, largura_etiqueta, altura_etiqueta, row, logo_path, championship, stage)
-            etiquetas_na_pagina += 1
-            
-            # Verificar se alcançou 10 etiquetas e mudar para uma nova página
-            if etiquetas_na_pagina >= 10:
-                c.showPage()
-                y_position = altura_pagina - margem_topo - altura_etiqueta
-                etiquetas_na_pagina = 0
-                linha = 0
-                break
+        desenhar_etiqueta(c, etiqueta_positions, y_position, largura_etiqueta, altura_etiqueta, row, logo_path, championship, stage)
+        etiquetas_na_pagina += 1
+        if etiqueta_positions == x_positions[0]:
+            etiqueta_positions = x_positions[1]
+        elif etiqueta_positions == x_positions[1]:
+            y_position = y_position - altura_etiqueta
+            etiqueta_positions = x_positions[0]
         
-        # Ajuste de posição vertical após desenhar ambas as etiquetas da linha
-        linha += 1
-        if linha < 5:
-            y_position -= altura_etiqueta
-        else:
-            linha = 0
+        if etiquetas_na_pagina >= 10:
+            c.showPage()
             y_position = altura_pagina - margem_topo - altura_etiqueta
+            etiquetas_na_pagina = 0
 
     c.save()
     pdf_data = buffer.getvalue()

ff8aaf7612459b612b4b16660b201a5980a495ed
sarah-inteceleri <sarah.vieira@gedu.demo.inteceleri.com.br>
2024-11-04 22:24:12 -0300

    feat: modularização

--- app.py
+++ app.py
@@ -1,30 +1,29 @@
 import streamlit as st
 
-from table_treatment_semec import main as verifica_main
-import table_treatment_moju
-import table_treatment_obidos
+import modules.semec.data_manipulation_semec as semec
+
 
 
 def set_initial_state():
     st.session_state.current_page = 'home'
 
-# Verificar se o estado atual existe, se não, inicializar
+# Verificar se o estado atual existe, ses não, inicializar
 if 'current_page' not in st.session_state:
     set_initial_state()
 
 st.title('Bem-vindo ao Criador de Etiquetas')
 
 if st.sidebar.button('Criação de Etiquetas - SEMEC'):
-    st.session_state.current_page = 'main'
+    st.session_state.current_page = 'SEMEC'
 
-if st.sidebar.button('Criação de Etiquetas - MOJU'):
-    st.session_state.current_page = 'MOJU'
+# if st.sidebar.button('Criação de Etiquetas - MOJU'):
+#     st.session_state.current_page = 'MOJU'
 
-if st.sidebar.button('Criação de Etiquetas - OBIDOS'):
-    st.session_state.current_page = 'OBIDOS'
+# if st.sidebar.button('Criação de Etiquetas - OBIDOS'):
+#     st.session_state.current_page = 'OBIDOS'
 
-if st.session_state.current_page == 'main':
-    verifica_main()
+if st.session_state.current_page == 'SEMEC':
+    semec.main()
 
 elif st.session_state.current_page == 'home':
     st.write('Por favor, escolha uma opção ao lado para criar as etiquetas.')

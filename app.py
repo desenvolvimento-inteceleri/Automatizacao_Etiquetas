c94a2edc101ae2ec0fc6e57912979aa9e11a9128
Sarah Vieira <sarah.vieira@gedu.demo.inteceleri.com.br>
2024-11-11 11:59:19 -0300

    feat: criação da pasta de Aldeias Altas

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import modules.semec.data_manipulation_semec as semec
 import modules.peixe_boi.data_manipulation_peixe_boi as peixe_boi
 import modules.altaneira_ce.data_manipulation_altaneira as altaneira
+import modules.aldeias_alta.data_manipulation_aldeias as aldeias_altas
 
 
 def set_initial_state():
@@ -23,6 +24,9 @@ if st.sidebar.button('Criação de Etiquetas - PEIXE BOI'):
 if st.sidebar.button('Criação de Etiquetas - ALTANEIRA'):
    st.session_state.current_page = 'ALTANEIRA'
 
+if st.sidebar.button('Criação de Etiquetas - ALDEIAS ALTAS'):
+   st.session_state.current_page = 'ALDEIAS ALTAS'
+
 if st.session_state.current_page == 'SEMEC':
     semec.main()
 
@@ -31,6 +35,9 @@ if st.session_state.current_page == 'PEIXE BOI':
 
 if st.session_state.current_page == 'ALTANEIRA':
     altaneira.main()
+
+if st.session_state.current_page == 'ALDEIAS ALTAS':
+    aldeias_altas.main()
 
 elif st.session_state.current_page == 'home':
     st.write('Por favor, escolha uma opção ao lado para criar as etiquetas.')
